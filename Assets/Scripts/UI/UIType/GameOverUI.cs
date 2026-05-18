c7d1650c83aa8c0cf0d8f1121a63b7b15961a5c4
Felia <felia1564@gmail.com>
2026-05-18 14:35:10 +0900

    UI 베이스 다듬기 전 중셉

---
+++ Assets/Scripts/UI/UIType/GameOverUI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameOverUI: UIBase
+{
+    [Header("UI 참조")]
+    [SerializeField] private Button Button_Restart;
+
+    private void Awake()
+    {
+        InitUIButton();
+        // 시작할 때는 UI가 보이지 않도록 꺼둠
+        this.gameObject.SetActive(false);
+    }
+
+    private void InitUIButton()
+    {
+        if (Button_Restart != null)
+        {
+            // 버튼 클릭 이벤트 바인딩
+            Button_Restart.onClick.AddListener(OnClickRestartGame);
+        }
+    }
+
+    // UIManager나 GameManager에서 이 UI를 띄울 때 호출할 함수
+    public void ShowVictoryUI()
+    {
+        this.gameObject.SetActive(true);
+
+        // 필요하다면 게임의 시간을 멈춤 (배경 움직임, 적 움직임 정지)
+        Time.timeScale = 0f;
+    }
+
+    // 버튼 클릭 시 호출되는 함수 (동사로 시작)
+    private void OnClickRestartGame()
+    {
+        // 정지했던 시간을 다시 원래대로 돌려놓음 (중요!)
+        Time.timeScale = 1f;
+
+        // 현재 활성화된 씬의 이름을 가져와서 다시 로드 = 게임 재시작
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    private void OnDestroy()
+    {
+        // 이벤트 해제 (메모리 누수 방지)
+        if (Button_Restart != null)
+        {
+            Button_Restart.onClick.RemoveListener(OnClickRestartGame);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIType/LobbyMenuUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LobbyMenuUI : UIBase
+{
+    [SerializeField] private UIButton Button_GameStart;
+    [SerializeField] private UIButton Button_GameQuit;
+
+
+    private void OnEnable()
+    {
+        Button_GameStart.BindOnClickButtonEvent(OnClick_GameStart);
+        Button_GameQuit.BindOnClickButtonEvent(OnClick_GameQuit);
+    }
+
+
+    public void OnClick_GameStart()
+    {
+        Debug.Log("게임 시작 버튼 작동");
+    }
+
+    public void OnClick_GameQuit()
+    {
+        Debug.Log("게임 종료 버튼 작동");
+        MotherBrain.Instance.QuitGame();
+    }
+}
